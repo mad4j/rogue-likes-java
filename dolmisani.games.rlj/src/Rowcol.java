15ffebd7449d7c3d6fa5bf65f4e73fbbb60138ce
mad4j <daniele.olmisani@gmail.com>
2013-08-26 09:12:26 +0200

    fisrt commit

---
+++ dolmisani.games.rlj/src/Rowcol.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+class Rowcol  implements Serializable {
+	private static final long serialVersionUID= 3L;
+	int row;
+	int col;
+	Rowcol(int r, int c){
+		row= r;
+		col= c;
+	}
+	Rowcol(){
+		this(0,0);
+	}
+	public String toString(){
+		return '[' + Integer.toString(row) + ' ' + Integer.toString(col) + ']';
+	}
+}
+
